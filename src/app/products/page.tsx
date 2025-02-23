b4495ef0a1a65204276b9266bd9a3607f171244a
Jorge Braga <117918908+jorgebragadev@users.noreply.github.com>
2025-02-23 11:40:14 -0300

    feat: add shadcn initial setud and components

---
+++ src/app/products/page.tsx
@@ -0,0 +1,14 @@
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+
+const ProductsPage = () => {
+    return (
+        <div>
+            <h1 className="text-red-500">Products Page</h1>
+            <Button>FWS 7.0</Button>
+            <Input placeholder="Bora fechar este projeto" />
+        </div>
+    );
+}
+
+export default ProductsPage;
